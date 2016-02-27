4ea357bd6dfbe859ba89a0b428faa2bbb05b5e5f
Juan Silva <Juan@Juan-PC.lan>
2016-02-26 19:55:07 -0600

    Se arregla por fin el problema del deploy, agregando manualmente en la consola de glassfish el pool connection y el resource jdbc. Se crearon los session beans for entity classes. se complia y ejecuta sin problemas.

---
+++ myApp3/src/java/myApp3/dao/MenuFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myApp3.dao;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import myApp3.models.Menu;
+
+/**
+ *
+ * @author Juan
+ */
+@Stateless
+public class MenuFacade extends AbstractFacade<Menu> {
+    @PersistenceContext(unitName = "myApp3PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public MenuFacade() {
+        super(Menu.class);
+    }
+    
+}
